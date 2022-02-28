115af2979518a63e538fec22fb21be18029c8bb5
Yeming <yb304@cam.ac.uk>
2022-02-28 23:58:46 +0000

    fix error in flood

--- floodsystem/plot.py
+++ floodsystem/plot.py
@@ -3,15 +3,12 @@ from datetime import datetime, timedelta
 from .station import MonitoringStation
 from .flood import stations_highest_rel_level
 
-t = [datetime(2022, 2, 19), datetime(2022, 2, 20), 
-    datetime(2022, 2, 21), datetime(2022, 2, 22), datetime[2022, 2, 23],
-    datetime[2022, 2, 24], datetime[2022, 2, 25], datetime[2022, 2, 26],
-    datetime[2022, 2, 27], datetime[2022, 2, 28]]
+
 
 def plot_water_levels(station, dates, levels):
    
     # Plot
-    plt.plot(t, levels)
+    plt.plot(dates, levels)
 
     # Add axis labels, rotate date labels and add plot title
     plt.xlabel('date')
