98f7f74c648405a43bc982d1802fff3a2bc53d8f
Yeming <yb304@cam.ac.uk>
2022-02-28 22:48:14 +0000

    task 2c

--- floodsystem/flood.py
+++ floodsystem/flood.py
@@ -12,6 +12,6 @@ def stations_level_over_threshold(stations, tol):
 def stations_highest_rel_level(stations, N):
     return sorted_by_key(
            [(station, station.relative_water_level()) 
-           for station in stations if station.relative_water_level() != None],
+           for station in stations if station.relative_water_level() != None], 1,
            reverse=True)[:N]
 
