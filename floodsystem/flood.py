2491ed0e80aaac5d9e48df268287ab9380f2c569
Yeming <yb304@cam.ac.uk>
2022-03-01 22:40:10 +0000

    edit 2C but can't fix 2E

--- floodsystem/flood.py
+++ floodsystem/flood.py
@@ -20,5 +20,5 @@ def stations_highest_rel_level(stations, N):
     sorted_stations_with_level = sorted_by_key(stations_with_level, 1, reverse=True)[:N]
 
     # [Station1 , Station2]
-    sorted_stations = [n[0] for n in sorted_stations_with_level]
-    return sorted_stations
+    #sorted_stations = [n[0] for n in sorted_stations_with_level]
+    return sorted_stations_with_level
