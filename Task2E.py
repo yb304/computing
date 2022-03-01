12e44bf7bee2105d01a77f00ea181af863d012cb
Yeming <yb304@cam.ac.uk>
2022-03-01 00:37:13 +0000

    task 2E

--- Task2E.py
+++ Task2E.py
@@ -5,13 +5,20 @@ from floodsystem.plot import plot_water_levels
 from datetime import timedelta
 
 def run():
+
     stations = build_station_list()
     update_water_levels(stations)
 
     highest_5 = stations_highest_rel_level(stations, 5)
+    dates = []
+    levels = []
     for station in highest_5:
-        plot_water_levels(station,
-        fetch_measure_levels(station.measure_id, timedelta(10)))
+        date, level = fetch_measure_levels(station.measure_id, timedelta(10))
+        dates.append(date)
+        levels.append(level)
+
+    plot_water_levels(highest_5, dates, levels)
+
 
 if __name__ == "__main__":
     print("*** Task 2E: CUED Part IA Flood Warning System ***")
