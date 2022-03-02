79720d1488d5b6a30908c58456a8f868850e7922
Luis Thiam-Nye <luis.tn@protonmail.ch>
2022-03-02 14:04:12 +0000

    Add Instrumentation

--- test_flood.py
+++ test_flood.py
@@ -1,12 +1,13 @@
 import sys
 import type_validation as tv
-from type_specs import rel_level_p
+from type_specs import rel_level_p, nat_int_p
 from floodsystem.stationdata import build_station_list, update_water_levels
 from floodsystem.flood import stations_level_over_threshold, stations_highest_rel_level
 from floodsystem.station import MonitoringStation, inconsistent_typical_range_stations
 
 
 _o_stations_level_over_threshold = stations_level_over_threshold
+_o_stations_highest_rel_level = stations_highest_rel_level
 
 
 def _i_stations_level_over_threshold(stations, tol):
@@ -17,7 +18,16 @@ def _i_stations_level_over_threshold(stations, tol):
     return stns
 
 
+def _i_stations_highest_rel_level(stations, N):
+    tv.assert_type(stations, (list, MonitoringStation))
+    tv.assert_type(N, nat_int_p)
+    stns = _o_stations_highest_rel_level(stations, N)
+    tv.assert_type(stns, (list, MonitoringStation))
+    return stns
+
+
 stations_level_over_threshold = _i_stations_level_over_threshold
+stations_highest_rel_level = _i_stations_highest_rel_level
 
 stations = build_station_list()
 update_water_levels(stations)
--- type_specs.py
+++ type_specs.py
@@ -3,6 +3,7 @@ import operator as op
 
 non_neg_p = partial(op.le, 0)
 non_empty_str_spec = ("and", [str, (lambda x: x)])
+nat_int_p = ("and", [int, non_neg_p])
 
 
 rel_level_p = ("and", [float])
