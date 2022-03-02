cddf58a1ce7c28a959ef2d490607c4beca80eeca
Luis Thiam-Nye <luis.tn@protonmail.ch>
2022-03-02 21:26:37 +0000

    Refine task 2c

--- Task2C.py
+++ Task2C.py
@@ -1,15 +1,14 @@
-from floodsystem.flood import stations_highest_rel_level
+from test_flood import stations_highest_rel_level
 from floodsystem.stationdata import build_station_list, update_water_levels
 
 
 def run():
-
     stations = build_station_list()
     update_water_levels(stations)
 
+    print("\nCurrent station relative water levels:")
     for station in stations_highest_rel_level(stations, 10):
-        print("Station {} has current water level: {}".format(
-            station.name, station.relative_water_level()))
+        print(f"{station.name:<50} {station.relative_water_level():>10}")
 
 
 if __name__ == "__main__":
